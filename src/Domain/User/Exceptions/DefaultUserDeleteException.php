ddc080df451d364b1d7a288120378102490bf03c
Szymon Rykała <szymon.rykala@atos.net>
2021-10-18 01:01:06 +0200

    fix exceptions base to 401 for bad creds

--- src/Domain/User/Exceptions/DefaultUserDeleteException.php
+++ src/Domain/User/Exceptions/DefaultUserDeleteException.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 namespace App\Domain\User\Exceptions;
 
-use App\Domain\Exception\DomainBadRequestException;
+use App\Domain\Exception\DomainForbiddenOperationException;
 
 
-class DefaultUserDeleteException extends DomainBadRequestException
+class DefaultUserDeleteException extends DomainForbiddenOperationException
 {
     public $message = "Nie można usunąć użytkownika z predefiniowaną klasą dostępu.";
 }
--- src/Domain/User/Exceptions/DeletedUserUpdateException.php
+++ src/Domain/User/Exceptions/DeletedUserUpdateException.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Domain\User\Exceptions;
 use App\Domain\Exception\DomainForbiddenOperationException;
 
+
 class DeletedUserUpdateException extends DomainForbiddenOperationException
 {
     public $message = "Nie można aktualizować usuniętego użytkownika.";
