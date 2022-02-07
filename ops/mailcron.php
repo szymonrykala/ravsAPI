41dc18cae6661309408bdea9d4bc37dfe72b43d5
Szymon Rykała <szymonrykala@gmail.com>
2022-02-08 00:16:11 +0100

    add mailer cron job

---
+++ ops/mailcron.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+
+$mail->setLanguage('pl');
+$mail->CharSet = 'UTF-8';
+// $mail->SMTPDebug  = getenv('SMTP_DEBUG');
+$mail->SMTPDebug  = 1;
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = getenv('SMTP_PORT');
+$mail->Host       = getenv('SMTP_HOST');
+$mail->Username   = getenv('SMTP_USER');
+$mail->Password   = getenv('SMTP_PASSWORD');
+
+$mail->IsHTML(true);
+$mail->SetFrom(getenv('SMTP_USER'), 'Ravs System Cron');
+
+$mail->addAddress('szymonrykala@gmail.com');
+$mail->Subject = 'Ravs System Cron job';
+$mail->msgHTML('<h1>Cron Job</h1>');
+
+$mail->send();
+
